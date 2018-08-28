40adcabb4c261ab53e7dc325f85ac85503aa4ed3
LOUTFI <LOUTFI@LOUTFI-PC>
2018-08-28 20:46:53 +0200

    modification page principal

--- ApiSocietaire/src/main/java/api/iard/societaire/SocietaireApplication.java
+++ ApiSocietaire/src/main/java/api/iard/societaire/SocietaireApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SocietaireApplication {
-
+	// page principal de notre api
 	public static void main(String[] args) {
 		SpringApplication.run(SocietaireApplication.class, args);
 	}
